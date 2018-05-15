c7c677b5296c6d2fc547ff8c4fc9c352178c3600
Kim Desrosiers <kimdesro@gmail.com>
2018-05-14 22:25:03 -0400

    Finish the first version for comparison

--- src/components/CssImplementation.vue
+++ src/components/CssImplementation.vue
@@ -1,13 +1,35 @@
 <template>
-    
+    <div>
+        <input type="checkbox" v-model="isActive"/>
+        <ul>
+            <li :class="{active: isActive}" v-for="n in 1000">
+                {{n}} <span class="li-span">(active)</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "CssImplementation"
+        name: "CssImplementation",
+        data() {
+            return {
+                isActive: true
+            }
+        },
     }
 </script>
 
-<style scoped>
+<style>
+    li.active {
+        color: #780000;
+    }
 
+    li.active .li-span {
+        display: inline-block;
+    }
+
+    li .li-span {
+        display: none;
+    }
 </style>
--- src/components/JsImplementation.vue
+++ src/components/JsImplementation.vue
@@ -1,13 +1,31 @@
 <template>
-    
+    <div>
+        <input type="checkbox" v-model="isActive"/>
+        <ul>
+            <li :class="{active: isActive}" v-for="n in 1000">
+                {{n}} <span v-show="isActive" class="li-span">(active)</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "CssImplementation"
+        name: "JsImplementation",
+        data() {
+            return {
+                isActive: true
+            }
+        },
     }
 </script>
 
-<style scoped>
+<style>
+    li.active {
+        color: #780000;
+    }
 
+    li .li-span {
+        display: inline-block;
+    }
 </style>
